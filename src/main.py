9cd8b64965d7c585c30059405f76b91abcda7db9
Ahortu901 <aricktech.edu@gmail.com>
2023-04-11 01:42:08 +0000

    update

---
+++ src/main.py
@@ -0,0 +1,11 @@
+"from src import create_app"
+from flask import Flask, render_template
+
+app= Flask(__name__)
+
+@app.route('/',strict_slashes=False, methods=['GET', 'POST'])
+def index():
+    return render_template('home.html')
+
+if __name__=='__main__':
+    app.run(debug=True)
