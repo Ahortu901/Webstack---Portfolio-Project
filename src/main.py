d08afbd33f1a082bf1011e017e44c0bd355c00ca
Ahortu901 <aricktech.edu@gmail.com>
2023-04-12 02:58:50 +0000

    update

--- src/main.py
+++ src/main.py
@@ -7,7 +7,7 @@ app= Flask(__name__)
 
 @app.route('/',strict_slashes=False, methods=['GET', 'POST'])
 def index():
-    return render_template('home.html')
+    return render_template('index.html')
 
 if __name__=='__main__':
     app.run(debug=True)
